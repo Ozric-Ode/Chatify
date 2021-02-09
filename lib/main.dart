55bb72da99e8b2b331bd8baef36f5537c0ccdf1a
Chirag Mathur <chiragmathurindia.com>
2021-02-09 15:34:43 +0530

    Add Email Verification system :heavy tick:

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,152 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:email_auth/email_auth.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _otp = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _sendOtp() async {
+    EmailAuth.sessionName = "Chatify";
+    var res = await EmailAuth.sendOtp(receiverMail: _email.text);
+    print('Helloooo');
+    print(res);
+    if (res) {
+      print('OTP Sent');
+    } else {
+      print('OTP not Sent');
+    }
+  }
+
+  void _verifyOtp() async {
+    var res =
+        await EmailAuth.validate(receiverMail: _email.text, userOTP: _otp.text);
+    if (res) {
+      print('OTP Verified');
+      _email.clear();
+      _otp.clear();
+      return showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: Icon(
+            Icons.verified,
+            color: Colors.green,
+          ),
+          content: Text('Email Verified !'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            )
+          ],
+        ),
+      );
+    } else {
+      print('OTP Not Verified');
+      _otp.clear();
+      return showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: Icon(
+            Icons.not_interested,
+            color: Colors.red,
+          ),
+          content: Text('Email Not Verified !'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            )
+          ],
+        ),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Chatify"),
+        backgroundColor: Colors.green,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        padding: EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.only(top: 50.0),
+                child: Center(
+                  child: Text(
+                    'Chatify',
+                    style: GoogleFonts.montserrat(
+                      textStyle: Theme.of(context).textTheme.headline4,
+                      color: Color(0xFF696969),
+                      fontSize: 64,
+                      fontWeight: FontWeight.w200,
+                      letterSpacing: -5,
+                    ),
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            TextField(
+              controller: _email,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  hintText: "Enter Email",
+                  labelText: "Email",
+                  suffixIcon: TextButton(
+                    child: Text("SEND OTP"),
+                    onPressed: _sendOtp,
+                  )),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            TextField(
+              controller: _otp,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                hintText: "Enter OTP",
+                labelText: "OTP",
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Center(
+              child: RaisedButton(
+                onPressed: () {
+                  _verifyOtp();
+                },
+                child: Text("Verify Email"),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
